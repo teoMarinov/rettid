c1930167544c5a7942eab2916794ebacf8b27c41
teoMarinov <teodor.b.marinov@gmail.com>
2024-01-22 19:40:13 +0200

    change sign up to work only with /user/signup/ in url

--- Api/index.php
+++ Api/index.php
@@ -7,15 +7,16 @@ header("Access-Control-Allow-Methods: *");
 require_once "./includes/signup.inc.php";
 
 $method = $_SERVER["REQUEST_METHOD"];
-$path = $_SERVER["REQUEST_URI"];
+$path = explode("/", $_SERVER["REQUEST_URI"]);
 
 switch ($method) { 
     case "POST":
-        $data = json_decode(file_get_contents("php://input"), 1);
-        $username = $data["username"];
-        $nickname = $data["nickname"];
-        $password = $data["password"];
-        $email = $data["email"];
-        echo $path;
-        signup($username, $nickname, $password, $email);
+        if ($path[3] === "users" && $path[4] === "signup") {
+            $data = json_decode(file_get_contents("php://input"), 1);
+            $username = $data["username"];
+            $nickname = $data["nickname"];
+            $password = $data["password"];
+            $email = $data["email"];
+            signup($username, $nickname, $password, $email);
+        }
 }
