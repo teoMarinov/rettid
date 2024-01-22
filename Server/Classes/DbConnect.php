bd1a5da8b81f61e7cbc316ccb0df8b75dccb3fb6
teoMarinov <teodor.b.marinov@gmail.com>
2024-01-22 14:12:45 +0200

    Add DB connection

---
+++ Server/Classes/DbConnect.php
@@ -0,0 +1,21 @@
+<?php
+
+class DbConnect
+{
+    private $server = "localhost";
+    private $port = ' 3308';
+    private $dbname = "rettid_db";
+    private $user = "root";
+    private $pass = "";
+
+    public function connect()
+    {
+        try {
+            $conn = new PDO("mysql:host=$this->server;port=$this->port;dbname=$this->dbname", $this->user, $this->pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Database Error: " . $e->getMessage();
+        }
+    }
+}
