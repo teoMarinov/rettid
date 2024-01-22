6efa445beb28bda297353255037ca3bccef21fb8
teoMarinov <teodor.b.marinov@gmail.com>
2024-01-22 23:14:45 +0200

    add error handles for the sign up form

--- rettid/src/components/Signup.tsx
+++ rettid/src/components/Signup.tsx
@@ -10,14 +10,15 @@ function Signup() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({
     "username taken": "",
-    "username too short": "",
-    "nickname too short": "",
+    "username length": "",
+    "nickname length": "",
     "email taken": "",
+    "invalid email": "",
     "password requirements": "",
     "passwords don't match": "",
   });
 
-  const usernameTaken = () => {
+  const usernameTaken = async () => {
     const url = "http://localhost/rettid/Api/users/check_username";
     const headers = new Headers({
       "Content-Type": "application/json",
@@ -28,7 +29,7 @@ function Signup() {
       body: JSON.stringify(username),
     };
 
-    fetch(url, requestOptions)
+    return fetch(url, requestOptions)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
@@ -36,68 +37,140 @@ function Signup() {
         return response.json();
       })
       .then((result) => {
-        if (result.exists) {
-          console.log("taken");
-          setErrors({
-            ...errors,
-            "username taken": "That username already exists",
-          });
-        } else {
-          console.log("not taken");
-          setErrors({
-            ...errors,
-            "username taken": "",
-          });
+        return result.exists;
+      })
+      .catch((error) => {
+        JSON;
+        console.error("Error!!!:", error.message);
+      });
+  };
+  const emailTaken = async () => {
+    const url = "http://localhost/rettid/Api/users/check_email";
+    const headers = new Headers({
+      "Content-Type": "application/json",
+    });
+    const requestOptions = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(email),
+    };
+    return fetch(url, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        return response.json();
+      })
+      .then((result) => {
+        return result.exists;
+      })
+      .catch((error) => {
+        JSON;
+        console.error("Error!!!:", error.message);
+      });
+  };
+  const checkPasswords = () => {
+    if (
+      password.length < 6 ||
+      password.length > 32 ||
+      !/[A-Z]/.test(password) ||
+      !/[0-9]/.test(password) ||
+      !/[!@#$%^&*(),.?":{}|<>]/.test(password)
+    ) {
+      return true;
+    }
+  };
+  const signUp = () => {
+    const form = {
+      username,
+      nickname,
+      password,
+      email,
+    };
+
+    const url = "http://localhost/rettid/Api/users/signup";
+
+    const headers = new Headers({
+      "Content-Type": "application/json",
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(form),
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
       })
       .catch((error) => {
         console.error("Error!!!:", error.message);
       });
   };
 
-  const checkForErrors = () => {
-    usernameTaken();
-  };
 
-  // const signUp = () => {
-  //   const form = {
-  //     username,
-  //     nickname,
-  //     password,
-  //     email,
-  //   };
 
-  //   const url = "http://localhost/rettid/Api/users/signup";
-
-  //   const headers = new Headers({
-  //     "Content-Type": "application/json",
-  //   });
-
-  //   const requestOptions = {
-  //     method: "POST",
-  //     headers: headers,
-  //     body: JSON.stringify(form),
-  //   };
-
-  //   fetch(url, requestOptions)
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error(`HTTP error! Status: ${response.status}`);
-  //       }
-  //       return response.json();
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error!!!:", error.message);
-  //     });
-  // };
-
-  const handleSubmit = () => {
-    checkForErrors();
-    if (errors["username taken"]) {
-      alert("Taken");
+  const handleSubmit = async () => {
+    const usernameExists = await usernameTaken();
+    const emailExists = await emailTaken();
+    if (usernameExists) {
+      setErrors({ ...errors, "username taken": "That username is taken!" });
+      return;
+    } else {
+      setErrors({ ...errors, "username taken": "" });
     }
-
-    // signUp();
+    if (emailExists) {
+      setErrors({ ...errors, "email taken": "That email is taken!" });
+      return;
+    } else {
+      setErrors({ ...errors, "email taken": "" });
+    }
+    if (username.length < 4 || username.length > 16) {
+      setErrors({
+        ...errors,
+        "username length": "Username must be between 4 and 16 characters!",
+      });
+      return;
+    } else {
+      setErrors({ ...errors, "username length": "" });
+    }
+    if (nickname.length < 4 || nickname.length > 16) {
+      setErrors({
+        ...errors,
+        "nickname length": "Nickname must be between 4 and 16 characters!",
+      });
+      return;
+    } else {
+      setErrors({ ...errors, "nickname length": "" });
+    }
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      setErrors({ ...errors, "invalid email": "Invalid email" });
+      return;
+    } else {
+      setErrors({ ...errors, "invalid email": "" });
+    }
+    if (checkPasswords()) {
+      setErrors({
+        ...errors,
+        "password requirements":
+          "Password must be between 6 and 32 have at least 1 upper case 1 number 1 special char!",
+      });
+    } else {
+      setErrors({ ...errors, "password requirements": "" });
+    }
+    if (password !== confirmPassword) {
+      setErrors({
+      ...errors,
+         "passwords don't match": "Passwords don't match!",
+      });
+    } else {
+      setErrors({...errors, "passwords don't match": "" });
+    }
+    signUp();
   };
 
   return (
