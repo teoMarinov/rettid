a1434a0832e0356d3339a09f1f4f83a804f1af24
teoMarinov <teodor.b.marinov@gmail.com>
2024-01-26 15:25:47 +0200

    add data type to subview

--- rettid/src/common/types.ts
+++ rettid/src/common/types.ts
@@ -1,12 +1,37 @@
 export type User = {
-    username: string,
-    nickname: string,
-    password: string,
-    email: string,
-    post_karma: number,
-    comment_karma: number,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    profile_picture: any,
-    is_admin: boolean,
-    is_banned: boolean
-}
+  username: string;
+  nickname: string;
+  password: string;
+  email: string;
+  post_karma: number;
+  comment_karma: number;
+  profile_picture: null;
+  is_admin: boolean;
+  is_banned: boolean;
+};
+
+export type Post = {
+  comment: number;
+  conter: string;
+  create_ad: string;
+  created_by: string;
+  likes: number;
+  dislikes: number;
+  id: number;
+  sub_title: string;
+  title: string;
+};
+
+export type Sub = {
+  created_at: string;
+  sub_ownder: string;
+  titles: string;
+};
+
+export type SubRettidData = {
+  bans: string[];
+  followers: string[];
+  mods: string[];
+  posts: Post[];
+  sub: Sub;
+};
--- rettid/src/components/SubView.tsx
+++ rettid/src/components/SubView.tsx
@@ -2,9 +2,10 @@ import { useLocation } from "react-router-dom"
 import { getSubInfo } from "../untils/getSubInfo";
 import { useEffect, useState } from "react";
 import { Button } from "@chakra-ui/react";
+import { SubRettidData } from "../common/types";
 export default function SubView() {
 
-  const [data, setData] = useState<any>()
+  const [data, setData] = useState<SubRettidData>()
 
   const subName = useLocation().pathname.split("/")[1];
 
